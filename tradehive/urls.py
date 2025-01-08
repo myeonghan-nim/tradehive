db8b80be6d8bfb43809c28d985f2be7733fcf95a
myeonghan-nim <hannim0828@gmail.com>
2025-01-09 00:57:41 +0900

    Delete: needless URI

--- tradehive/urls.py
+++ tradehive/urls.py
@@ -20,6 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("session_security/", include("session_security.urls")),
     path("users/", include("users.urls")),
 ]
