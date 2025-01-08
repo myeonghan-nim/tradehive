dc22ded218be9fa59613affa201432801d9ca0e3
myeonghan-nim <hannim0828@gmail.com>
2025-01-09 00:58:02 +0900

    Add: needed model setting

--- users/models.py
+++ users/models.py
@@ -6,5 +6,8 @@ class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
 
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
     def __str__(self):
         return self.username
