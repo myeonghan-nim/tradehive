c8a1bf98f6182ab561ce664c65f960ca9f5c6882
myeonghan-nim <hannim0828@gmail.com>
2025-01-15 20:56:44 +0900

    Add: MFA for user

--- users/models.py
+++ users/models.py
@@ -1,13 +1,22 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django_otp.plugins.otp_totp.models import TOTPDevice
 
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
+    mfa_enabled = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
 
     def __str__(self):
         return self.username
+
+
+class CustomUserTOTPDevice(TOTPDevice):
+    pass
+
+    def __str__(self):
+        return f"{self.user.username} - {self.name}"
