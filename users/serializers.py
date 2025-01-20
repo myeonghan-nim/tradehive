97b11cd8f1db57d364aad4d71cfe99c7932d40b3
myeonghan-nim <hannim0828@gmail.com>
2025-01-20 20:14:34 +0900

    Change: serializer logic

--- users/serializers.py
+++ users/serializers.py
@@ -8,23 +8,26 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 from .models import CustomUserTOTPDevice
 
+PHONE_NUMBER_VALIDATOR = RegexValidator(regex=r"^\d{3}-\d{4}-\d{4}$", message="Phone number must be in the format xxx-xxxx-xxxx.")
+
 User = get_user_model()
+
+
+def validate_authorization_header(request):
+    auth_header = request.headers.get("Authorization")
+    if not auth_header or not auth_header.startswith("Bearer "):
+        raise serializers.ValidationError("Refresh token not provided in Authorization header.")
+    return auth_header.split(" ")[1]
 
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
     confirm_password = serializers.CharField(write_only=True, min_length=8)
-    phone_number = serializers.CharField(validators=[RegexValidator(regex=r"^\d{3}-\d{4}-\d{4}$", message="Phone number must be in the format xxx-xxxx-xxxx.")], required=False)
+    phone_number = serializers.CharField(validators=[PHONE_NUMBER_VALIDATOR], required=False)
 
     class Meta:
         model = User
-        fields = (
-            "email",
-            "username",
-            "password",
-            "confirm_password",
-            "phone_number",
-        )
+        fields = ("email", "username", "password", "confirm_password", "phone_number")
 
     def validate(self, data):
         if User.objects.filter(email=data["email"]).exists():
@@ -48,10 +51,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data.pop("confirm_password")
         user = User.objects.create_user(
-            email=validated_data["email"],
-            username=validated_data["username"],
-            password=validated_data["password"],
-            phone_number=validated_data.get("phone_number"),
+            email=validated_data["email"], username=validated_data["username"], password=validated_data["password"], phone_number=validated_data.get("phone_number")
         )
         return user
 
@@ -63,31 +63,20 @@ class LoginSerializer(serializers.Serializer):
 
 class LogoutSerializer(serializers.Serializer):
     def validate(self, data):
-        request = self.context["request"]
-
-        auth_header = request.headers.get("Authorization")
-        if not auth_header or not auth_header.startswith("Bearer "):
-            raise serializers.ValidationError("Refresh token not provided in Authorization header.")
-
-        refresh_token = auth_header.split(" ")[1]
+        request = self.context.get("request")
+        refresh_token = validate_authorization_header(request)
         try:
             token = RefreshToken(refresh_token)
             token.blacklist()
         except Exception as e:
             raise serializers.ValidationError(f"Invalid token: {str(e)}")
-
         return data
 
 
 class CustomTokenRefreshSerializer(serializers.Serializer):
     def validate(self, data):
-        request = self.context["request"]
-
-        auth_header = request.headers.get("Authorization")
-        if not auth_header or not auth_header.startswith("Bearer "):
-            raise serializers.ValidationError("Refresh token not provided in Authorization header.")
-
-        refresh_token = auth_header.split(" ")[1]
+        request = self.context.get("request")
+        refresh_token = validate_authorization_header(request)
         try:
             refresh = RefreshToken(refresh_token)
         except Exception:
@@ -100,7 +89,34 @@ class CustomTokenRefreshSerializer(serializers.Serializer):
         return data
 
 
-class EnableMFASerializer(serializers.Serializer):
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(write_only=True)
+    new_password = serializers.CharField(write_only=True, min_length=8)
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop("user", None)
+        super().__init__(*args, **kwargs)
+
+    def validate(self, data):
+        if not self.user:
+            raise serializers.ValidationError("User not found.")
+
+        if not self.user.check_password(data.get("old_password")):
+            raise serializers.ValidationError("Old password is incorrect.")
+
+        try:
+            validate_password(data.get("new_password"), user=self.user)
+        except ValidationError as e:
+            raise serializers.ValidationError({"password": list(e.messages)})
+
+        return data
+
+    def save(self):
+        self.user.set_password(self.validated_data.get("new_password"))
+        self.user.save()
+
+
+class EnableMFASerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUserTOTPDevice
         fields = ("name",)
@@ -112,9 +128,24 @@ class EnableMFASerializer(serializers.Serializer):
 
         if CustomUserTOTPDevice.objects.filter(user=user).exists():
             raise serializers.ValidationError("MFA is already enabled for this user.")
-        else:
-            user.mfa_enabled = True
-            user.save()
+
+        CustomUserTOTPDevice.objects.create(user=user, name=data.get("name"))
+        user.mfa_enabled = True
+        user.save()
+
+        return data
+
+
+class DeleteMFASerializer(serializers.Serializer):
+    def validate(self, data):
+        user = self.context.get("request").user
+        device = CustomUserTOTPDevice.objects.filter(user=user).first()
+        if not device:
+            raise serializers.ValidationError("MFA is not enabled for this user.")
+
+        device.delete()
+        user.mfa_enabled = False
+        user.save()
 
         return data
 
@@ -155,24 +186,26 @@ class VerifyOTPSerializer(serializers.Serializer):
         return data
 
 
-class DeleteMFASerializer(serializers.Serializer):
-    def validate(self, data):
-        user = self.context.get("request").user
-        device = CustomUserTOTPDevice.objects.filter(user=user).first()
-        if not device:
-            raise serializers.ValidationError("MFA is not enabled for this user.")
-
-        device.delete()
-        user.mfa_enabled = False
-        user.save()
-
-        return data
-
-
 class UserProfileSerializer(serializers.ModelSerializer):
-    phone_number = serializers.CharField(validators=[RegexValidator(regex=r"^\d{3}-\d{4}-\d{4}$", message="Phone number must be in the format xxx-xxxx-xxxx.")], required=False)
+    phone_number = serializers.CharField(validators=[PHONE_NUMBER_VALIDATOR], required=False)
 
     class Meta:
         model = User
         fields = ("email", "username", "phone_number")
         read_only_fields = ("email",)
+
+
+class UserProfileDeleteSerialzier(serializers.Serializer):
+    otp_code = serializers.CharField(write_only=True, max_length=6)
+
+    def validate(self, data):
+        user = self.context.get("request").user
+        try:
+            totp_device = CustomUserTOTPDevice.objects.get(user=user)
+        except CustomUserTOTPDevice.DoesNotExist:
+            raise serializers.ValidationError("MFA is not enabled for this user.")
+
+        if not totp_device.verify_token(data.get("otp_code")):
+            raise serializers.ValidationError("Invalid OTP.")
+
+        return data
