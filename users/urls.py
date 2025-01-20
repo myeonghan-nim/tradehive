c93aa470723dc1a1651d59fb9857c36e7bf54ba3
myeonghan-nim <hannim0828@gmail.com>
2025-01-20 20:15:00 +0900

    Add: new URI

--- users/urls.py
+++ users/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path
 
-from .views import RegisterView, LoginView, LogoutView, CustomTokenRefreshView, EnableMFAView, QRCodeView, VerifyOTPView, DeleteMFAView, UserProfileView
+from .views import RegisterView, LoginView, LogoutView, CustomTokenRefreshView, MFAView, QRCodeView, VerifyOTPView, UserProfileView, ChangePasswordView
 
 urlpatterns = [
-    # login, logout, and refresh
+    # login, logout, refresh, change password
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("refresh/", CustomTokenRefreshView.as_view(), name="refresh"),
+    path("change-password/", ChangePasswordView.as_view(), name="change_password"),
     # MFA
-    path("enable-mfa/", EnableMFAView.as_view(), name="enable-mfa"),
+    path("enable-mfa/", MFAView.as_view(), name="enable-mfa"),
+    path("delete-mfa/", MFAView.as_view(), name="delete-mfa"),
     path("qr-code/", QRCodeView.as_view(), name="qr-code"),
     path("verify-otp/", VerifyOTPView.as_view(), name="verify-otp"),
-    path("delete-mfa/", DeleteMFAView.as_view(), name="delete-mfa"),
     # profile
     path("profile/", UserProfileView.as_view(), name="profile"),
 ]
