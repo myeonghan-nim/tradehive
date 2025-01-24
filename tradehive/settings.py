643721c2c87b7a6df91a69d3c789fd0c12a3c8f9
myeonghan-nim <hannim0828@gmail.com>
2025-01-24 14:12:24 +0900

    Add: markets application

--- tradehive/settings.py
+++ tradehive/settings.py
@@ -68,6 +68,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt.token_blacklist",
     "users",
     "orders",
+    "markets",
 ]
 
 MIDDLEWARE = [
--- tradehive/urls.py
+++ tradehive/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
     path("orders/", include("orders.urls")),
+    path("markets/", include("markets.urls")),
 ]
