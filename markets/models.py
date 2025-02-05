aeafbf40ddb131fb138abd3a79bf0ab3e18470f2
myeonghan-nim <hannim0828@gmail.com>
2025-02-05 17:06:58 +0900

    Add: cryptocurrency model and related model

--- markets/models.py
+++ markets/models.py
@@ -1,9 +1,23 @@
 from django.db import models
 
 
+class CryptoCurrency(models.Model):
+    name = models.CharField(max_length=50)
+    symbol = models.CharField(max_length=10)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "cryptocurrencies"
+
+    def __str__(self):
+        return self.name
+
+
 class TradingPair(models.Model):
-    base_asset = models.CharField(max_length=10)
-    quote_asset = models.CharField(max_length=10)
+    base_asset = models.ForeignKey(CryptoCurrency, on_delete=models.CASCADE, related_name="base_asset")
+    quote_asset = models.ForeignKey(CryptoCurrency, on_delete=models.CASCADE, related_name="quote_asset")
     min_price = models.DecimalField(max_digits=20, decimal_places=8)
     max_price = models.DecimalField(max_digits=20, decimal_places=8)
     tick_size = models.DecimalField(max_digits=20, decimal_places=8)
