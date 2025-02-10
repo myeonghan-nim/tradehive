0049126c093587b5f3df38f92653a0748e3a1c04
myeonghan-nim <hannim0828@gmail.com>
2025-02-10 21:10:36 +0900

    Add: gunicorn

---
+++ conf/gunicorn/config.py
@@ -0,0 +1,12 @@
+import multiprocessing
+import os
+
+bind = "0.0.0.0:8000"
+
+workers = min(multiprocessing.cpu_count(), int(os.getenv("GUNICORN_WORKERS", 4))) * 2
+threads = int(os.getenv("GUNICORN_THREADS", 1))
+
+loglevel = os.getenv("GUNICORN_LOG_LEVEL", "info")
+
+certfile = "/app/cert.crt"
+keyfile = "/app/cert.key"
