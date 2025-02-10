1f262c7803c4340436bdaf5f814792c488132ff5
myeonghan-nim <hannim0828@gmail.com>
2025-02-10 21:11:03 +0900

    Change: run server type

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-python manage.py makemigrations
-python manage.py migrate
-
 if [ "$1" = "worker" ]; then
     echo "Starting Celery worker..."
     celery -A conf.celery worker --loglevel=info
@@ -12,5 +9,9 @@ elif [ "$1" = "beat" ]; then
     celery -A conf.celery beat --loglevel=info --scheduler django_celery_beat.schedulers.DatabaseScheduler
 else
     echo "Starting Django server..."
-    python manage.py runserver_plus 0.0.0.0:8000 --cert-file cert.crt --key-file cert.key
+
+    python manage.py makemigrations
+    python manage.py migrate
+
+    gunicorn -c conf/gunicorn/config.py tradehive.wsgi:application
 fi
