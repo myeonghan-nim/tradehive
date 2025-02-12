30edae55b56650b44d67da071c6389e0bd965eae
myeonghan-nim <hannim0828@gmail.com>
2025-02-12 20:36:23 +0900

    Add: websocket for price

---
+++ orders/consumers.py
@@ -0,0 +1,21 @@
+import json
+
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+
+class TradeConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.symbol = self.scope["url_route"]["kwargs"]["symbol"]
+        self.group_name = f"orders_{self.symbol}"
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
+
+    async def receive(self, text_data):
+        pass
+
+    async def send_trade_data(self, event):
+        trade_data = event["data"]
+        await self.send(text_data=json.dumps(trade_data))
--- tradehive/asgi.py
+++ tradehive/asgi.py
@@ -9,8 +9,16 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
+import orders.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tradehive.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": URLRouter(orders.routing.websocket_urlpatterns),
+    }
+)
