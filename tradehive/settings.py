42916c54f5be30e2a053674739baddbaac6f346c
myeonghan-nim <hannim0828@gmail.com>
2025-02-24 18:02:20 +0900

    Add: SSL on DB

--- tradehive/settings.py
+++ tradehive/settings.py
@@ -143,6 +143,9 @@ DATABASES = {
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": os.getenv("POSTGRES_HOST"),
         "PORT": os.getenv("POSTGRES_PORT"),
+        "OPTIONS": {
+            "sslmode": "require",
+        },
     }
 }
 
