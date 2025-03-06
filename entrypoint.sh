c41e9d31c7eb05f0ef6459c4027f0c7eb8f31911
myeonghan-nim <hannim0828@gmail.com>
2025-03-07 04:28:32 +0900

    Add: annotation about docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ if [ "$1" = "worker" ]; then
     celery -A conf.celery worker --loglevel=info
 elif [ "$1" = "beat" ]; then
     echo "Starting Celery Beat..."
-    celery -A conf.celery beat --loglevel=info --scheduler django_celery_beat.schedulers.DatabaseScheduler
+    celery -A conf.celery beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler
 else
     echo "Starting Django server..."
 
